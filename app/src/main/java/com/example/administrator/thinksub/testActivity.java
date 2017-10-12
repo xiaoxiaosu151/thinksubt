78d3d8f0181011ac1275b94b5a7c9fcaaa8d11d8
xiaoxiaosu151 <282612325@qq.com>
2017-10-12 17:08:44 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/thinksub/testActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.thinksub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+
+public class testActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
